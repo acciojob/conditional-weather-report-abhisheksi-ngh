a47bb3b2c7dbbaf4dbab968755110c7f0c3b7c55
abhishekashj <abhishekkumrsingh9585@gmail.com>
2025-03-26 22:39:13 +0530

    one file added some chnages in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-
 import React from "react";
 import './../styles/App.css';
+import WeatherDisplay from './WeatherDisplay';
 
 const App = () => {
+  const [weather, setWeather] = useState({ temperature: 0, conditions: "" });
+
+  useEffect(() => {
+    // Default weather data
+    setWeather({ temperature: 25, conditions: "Sunny" });
+  }, []);
   return (
     <div>
         {/* Do not remove the main div */}
+       <h1>Weather App</h1>
+      <WeatherDisplay weather={weather} />
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WeatherDisplay = ({ weather }) => {
+  const tempColor = weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+      <p style={{ color: tempColor }}>Temperature: {weather.temperature}</p>
+      <p>Conditions: {weather.conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
